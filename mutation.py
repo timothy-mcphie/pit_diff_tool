5afc357bfd0fe60588208a0172e0da3103f1636a
timothy-mcphie <mcphietimothy65@gmail.com>
2017-04-06 18:44:52 +0100

    using sets lol

--- mutation.py
+++ mutation.py
@@ -1,18 +1,3 @@
-class Mutation_score:
-    """
-    Helper class to store results of a mutation report
-    """
-
-    def __init__(self, name, total_mutants=0, killed=0, survived=0, no_coverage=0):
-        self.name=str(name)
-        self.total_mutants=total_mutants
-        self.killed=killed
-        self.survived=survived
-        self.no_coverage=no_coverage
-
-    def __str__(self):
-        return "[SCORE] Name: "+self.name+" Total mutants: "+str(self.total_mutants)+" Killed: "+str(self.killed)+" No Coverage: "+str(self.no_coverage)
-
 class Mutant:
     """
     Helper class to store information of each mutation test performed by pit (this is gleamed from the pit XML report)
@@ -27,23 +12,22 @@ class Mutant:
         self.method_description = str(method_description)
         self.line_no = str(line_no)
         self.mutator = str(mutator)
-        self.index = str(index)#this is the index to the first instruction on which the mutation occurs, it is specific to how ASM represents bytecode (MutationDetails.java:229)
+        self.index = str(index)#index to the first instruction where the mutation occurs, specific to ASM/bytecode representation (pit/MutationDetails.java:229)
         self.killing_test = str(killing_test)
         self.description = str(description) 
-        self.line = None
+        self.target_line_no = None
 
-    def key(self):
+    def __key(self):
         """
         Used as a key to uniquely determine a mutation.
         """
-        return self.source_file + \
-        self.mut_class + \
-        self.mut_method + \
-        self.method_description + \
-        self.mutator + \
-        self.description + \
-        self.killing_test + \
-        self.line_no
+        return self.source_file + self.line_no + self.mutator
+
+    def __hash__(self):
+        """
+        Used in sets
+        """
+        return hash(self.__key())
 
     def __str__(self):
         """
@@ -51,3 +35,119 @@ class Mutant:
         """
         return "[MUTANT] Detected: "+self.detected+" Status: "+self.status+" Src File: "+self.source_file+" Class: "+self.mut_class+ \
                 " Method: "+self.mut_method+" Mutator: "+self.mutator+" Description: "+self.description+" Lineno: "+self.line_no+" Killing test: "+self.killing_test
+
+class Score:
+    """
+    Store results of a set of mutants
+    """
+
+    def __init__(self, name, mutants=0, killed=0, survived=0, no_coverage=0):
+        self.name=name
+        self.mutants=mutants
+        self.killed=killed
+        self.survived=survived
+        self.no_coverage=no_coverage
+
+    def __str__(self):
+        return self.name+" Total mutants: "+str(self.mutants)+" Killed: "+str(self.killed)+" No Coverage: "+str(self.no_coverage)
+
+    def update(self, mutant):
+        """
+        Update score given a mutant
+        """
+        self.mutants += 1
+        if mutant.status == "no_coverage":
+            self.no_coverage += 1
+        elif mutant.status == "survived":
+            self.survived += 1
+        elif mutant.status == "killed": 
+            self.killed += 1
+
+class Mutation_score:
+    def __init__(self, name):
+        self.changed = Score("[CHANGED] "+name)
+        self.new = Score("[NEW] "+name)
+        self.unchanged = Score("[UNCHANGED] "+name)
+        self.removed = Score("[REMOVED] "+name)
+
+class Method_score(Mutation_score):
+    """
+    Store scores for new/changed mutants across a method.
+    """
+    def __init__(self, name):
+        self.__init__(name)
+        self.mutants = []
+
+    def update_new(self, mutant):
+        self.new.update(mutant)
+        self.mutants.append(mutant)
+
+    def update_changed(self, mutant):
+        self.changed.update(mutant)
+        self.mutants.append(mutant) 
+
+class Class_score(Mutation_score):
+    """
+    Store scores for new/changed mutants across a class.
+    """
+    def __init__(self, name):
+        self.__init__(name)
+        methods = {}
+
+    def update_new(self, mutant):
+        if mutant.mut_method not in self.methods:
+            self.methods[mutant.mut_method] = Class_score(mutant.mut_method)
+        self.new.update(mutant)
+        self.methods[mutant.mut_method].update_new(mutant)
+            
+    def update_changed(self, mutant):
+        if mutant.mut_method not in self.methods:
+            self.methods[mutant.mut_method] = Class_score(mutant.mut_method)
+        self.changed.update(mutant)
+        self.methods[mutant.mut_method].update_changed(mutant) 
+
+class File_score(Mutation_score):
+    """
+    Store scores for new/changed mutants across a source file..
+    """
+    def __init__(self, name):
+        self.__init__(name)
+        self.classes = {}
+
+    def update_new(self, mutant):
+        if mutant.mut_class not in self.classes:
+            self.classes[mutant.mut_class] = Class_score(mutant.mut_class)
+        self.new.update(mutant)
+        self.classes[mutant.mut_class].update_new(mutant)
+            
+    def update_changed(self, mutant):
+        if mutant.mut_class not in self.classes:
+            self.classes[mutant.mut_class] = Class_score(mutant.mut_class)
+        self.changed.update(mutant)
+        self.classes[mutant.mut_class].update_changed(mutant)
+
+class Report_score(Mutation_score):
+    """
+    Store scores for new/changed mutants across a report.
+    """
+    def __init__(self, name):
+        self.__init__(name)
+        self.files = {}
+
+    def add_file(self, filename):
+        self.files[filename] = File_score()
+
+    def get_file_score(self, filename):
+        self.files[filename] = File_score() 
+
+    def update_new(self, mutant):
+        if mutant.source_file not in self.files:
+            self.files[mutant.source_file] = File_score(mutant.source_file)
+        self.new.update(mutant)
+        self.files[mutant.source_file].update_new(mutant)
+            
+    def update_changed(self, mutant):
+        if mutant.source_file not in self.files:
+            self.files[mutant.source_file] = File_score(mutant.source_file)
+        self.changed.update(mutant)
+        self.files[mutant.source_file].update_changed(mutant)
--- pit_diff.py
+++ pit_diff.py
@@ -1,105 +1,103 @@
 import xml.etree.ElementTree as ET
 import sys
 import parser
+import fnmatch
 from mutation import Mutant, Mutation_score
 
-gitinfo = {}
+def update_mutant(mutant, modified_files):
+    """
+    Update a mutant in the new report to have the correct line numbers by parsing the hunks in it's patchedfile
+    an old_line_no is the new_line_no - (number of lines added < new_line_no) + (number of lines deleted < new_line_no)
+    """
+    source_file = mutant.source_file
+    if source_file not in modified_files:
+        return
+    target_to_source_dict, target_to_source_list = modified_files[source_file]
+    if mutant.line_no in target_to_source_dict:
+        mutant.target_line_no = mutant.line_no
+        mutant.line_no = target_to_source_dict[mutant.line_no].source_line_no 
+        #A new mutant is updated to None, will be included in the delta (no original mutant has None in its key)
+        return
+    iterator = iter(target_to_source_list)
+    line = iterator.next()
+    while mutant.line_no > line.target_line_no:
+        try:
+            next_line = iterator.next()
+            if mutant.line_no < next_line.target_line_no:
+                break 
+            line = next_line
+        except StopIteration as e:
+            print "Line is after all hunks, last hunk line:", line.target_line_no, " mutant line:", mutant.line_no
+            break 
+    mutant.target_line_no = mutant.line_no
+    mutant.line_no = mutant.line_no + (line.source_line_no - line.target_line_no) 
 
 def parse_report(filename):
     """
     Returns the root tag of an XML file
     """
+    #TODO: Add error handling
     tree = ET.parse(filename)
     root = tree.getroot()
     return root
 
-def update_mutation_score(score, mutant):
+def process_report(report, commit, modified_files, report_score, new=False):
     """
-    Helper method, given mutation_score object and a mutant, updates the score accordingly
+    Traverses a pit XML report - returns a set of unique mutants and a score
     """
-    score.total_mutants += 1
-    if mutant.status == "no_coverage":
-        score.no_coverage += 1
-    elif mutant.status == "survived":
-        score.survived += 1
-    elif mutant.status == "killed": 
-        score.killed += 1
-
-def process_report(report, new=False):
-    """
-    Traverses a pit XML file - returns either a dictionary of unique mutants and a score object, or a list of unique mutants and a score object.
-    """
-    #TODO: we need to update old files to their new line numbers!
-    #update_mutant(mutant, modified_files)
-    mutant_dict = {}
+    mutant_set = {}
     root = parse_report(report) 
-    score = Mutation_score(report)
-    repeats = 0
-    if new:
-        mutant_list = []
     for child in root:
-        """
-        store from XML into an object
-        Object tuples: detected, status, source_file, mutated_class, mutated_method, method_description, line_no, mutator, index, killing_test, description
-        """
         mutant = Mutant(child.attrib.get("detected"), child.attrib.get("status"), child[0].text, child[1].text, child[2].text, child[3].text, child[4].text, \
                 child[5].text, child[6].text, child[7].text,  child[8].text)
-        key = mutant.key()
-        if key not in mutant_dict:
-            #the mutant has not been seen
-            mutant_dict[key] = mutant 
-        else:
-            #mutant has been seen on this line number before - only different due to ASM index
-            repeats += 1 
-            continue
-        update_mutation_score(score, mutant)
         if new:
-            mutant_list.append(mutant)
-    if new:
-        return (mutant_list, score)
-    return (mutant_dict, score)
+            update_mutant(mutant, modified_files) 
+        if mutant not in mutant_set:
+            mutant.add(mutant)
+    return mutant_set
 
-def get_differences(old_rep, new_rep):
-    modified_files = parser.process_git_info("HEAD^", "HEAD") 
-    #TODO: - testing purposes - if using head use rev-parse to get has for use in file operations
-    #TODO: change this to two commits predetermined - check them out and run pit on them - hardcode paths into old_rep, new_rep
-    (mutant_dict, old_score) = process_report(old_rep)
-    (mutant_list, new_score) = process_report(new_rep, True)
-    diff_score = Mutation_score(old_rep+new_rep)
-    new_mutants = []
-    changed_mutants = []
+def get_differences(old_report, new_report, old_commit, new_commit):
+    report_score = Report_score(old_commit+new_commit)
+    #modified_files = parser.process_git_info("HEAD^", "HEAD", repo_path) 
+    old_set = process_report(old_report, old_commit, modified_files)
+    new_set = process_report(new_report, new_commit, modified_files, True)
+    #TODO: Use sets, gives us easily the new mutants, deleted mutants, changed and unchanged
+    for mutant in old_set.difference(new_set):
+        report_score.update_removed(mutant)
+
     for mutant in mutant_list:
         key = mutant.key()
-        update_mutant(mutant, modified_files) 
         if key in mutant_dict:
-            old_mutant = mutant_dict[key] #we've seen this mutant before 
+            old_mutant = mutant_dict[key]
             if mutant.status != old_mutant.status or mutant.detected != old_mutant.detected:
-                changed_mutants.append(str(mutant)) #a changed mutant - this is part of the delta
-                update_mutation_score(diff_score, mutant) 
+                report_score.update_changed(mutant)
         else:
-            #we haven't seen this mutant before, this is part of the "delta"
-            new_mutants.append(str(mutant))
-            update_mutation_score(diff_score, mutant)
-    differences = new_mutants + changed_mutants
-    return (differences, diff_score)
+            report_score.update_new(mutant)
+    return report_score
 
-#TODO: How to get the pit report names?
-old_rep = "/Users/tim/Code/commons-collections/pitReports/201703191437/mutations.xml"
-new_rep = "/Users/tim/Code/commons-collections/pitReports/201703191437/mutations.xml"
-usage_string = "python parser.py [old_report_path] [new_report_path]"
+def parse_report_score
 
 """
 Main
+#TODO: use the python arguments module
+usage_string = "python parser.py old_report_path new_report_path repo_path old_commit [new_commit]"
 if len(sys.argv) != 3 or sys.argv[2] < 1:
   print usage_string
-  #TODO: could this be problematic for calling scripts?... Integrate into a project for stats part
   sys.exit(1)
 old_rep = str(sys.argv[1])
 new_rep = str(sys.argv[2])
-TODO: add checks for files being XML files - introduce error checking into reports
-(differences, diff_score) = get_differences(old_rep, new_rep)
-print "DELTA ", str(diff_score)
+if not fnmatch.fnmatch(old_rep, "*.xml"): 
+    print sys.stderr, "Old report ", old_rep, " is not named as an XML file"
+if not fnmatch.fnmatch(new_rep, "*.xml"):
+    print sys.stderr, "New report ", new_rep, " is not named as an XML file"
+repo_path = str(sys.argv[3])
+old_commit = str(sys.argv[4])
+new_commit = str(sys.argv[5]) #optional can just pass an old_commit to compare with current HEAD
 """
 
-(differences, diff_score) = get_differences(old_rep, new_rep)
-print "Differences in mutations is: ", str(diff_score)
+#TODO: change this to two commits predetermined - check them out and run pit on them - hardcode paths into old_rep, new_rep
+#TODO: if using head use rev-parse to get hash for use in file operations
+old_rep = "/Users/tim/Code/commons-collections/pitReports/201703191437/mutations.xml"
+new_rep = "/Users/tim/Code/commons-collections/pitReports/201703191437/mutations.xml"
+report_score = get_differences(old_rep, new_rep, "HEAD^", "HEAD")
+print "DELTA ", str(report_score)
