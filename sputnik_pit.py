98bad3529552d45cda23afbdc19d5401070a5986
timothy-mcphie <mcphietimothy65@gmail.com>
2017-04-17 12:13:58 +0100

    test pit script writes input arg to a file

--- sputnik_pit.py
+++ sputnik_pit.py
@@ -1,6 +1,18 @@
+import sys
+import os
+import csv
+from pit_diff import cmd, diff, git_diff, scores as s
+
+def output_score():
+    with open("PIT_WORKED.TXT", "a+") as f:
+        w = csv.writer(f, delimiter=",")
+        w.writerow([1,2,3,4,5])
+        w.writerow(pit_filter)
+
 #repo is directory that jenkins is in
 pit_filter = str(sys.argv[1])
-echo "we have ", pit_filter, " as pit_filter"
+print "we have ", pit_filter, " as pit_filter"
+output_score()
 
 #Sputnik script calls rev-parse HEAD^ - if fails no prev commits
 #extract cp maven
